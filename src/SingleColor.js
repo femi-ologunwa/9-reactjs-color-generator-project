df7a6ed1df02a8efa230d90ed1bedb4b720efa66
Femi <ologunwafemi@gmail.com>
2021-05-07 12:41:29 +0100

    added functioanlity to be able to copy color values to clipboard

--- src/SingleColor.js
+++ src/SingleColor.js
@@ -9,13 +9,32 @@ const SingleColor = ({ rgb, weight, index, hexColor }) => {
 
    //const hex = rgbToHex(...rgb);
 
+   const hexValue = `#${hexColor}`;
+
+   //effect to clear alert text after 3 secs
+   useEffect(() => {
+      const timeout = setTimeout(() => {
+         setAlert(false);
+      }, 3000);
+      return () => {
+         clearTimeout(timeout);
+      };
+   }, [alert]);
+
    return (
       <article
          className={`color ${index > 10 && 'color-light'}`}
          style={{ backgroundColor: `rgb(${bcg})` }}
+         onClick={() => {
+            setAlert(true);
+
+            //use javascript function to copy value to clicpboard
+            navigator.clipboard.writeText(hexValue);
+         }}
       >
          <p className='percent-value'>{weight}%</p>
-         <p className='color-value'>{hexColor}</p>
+         <p className='color-value'>{hexValue}</p>
+         {alert && <p className='alert'>copied to clipboard</p>}
       </article>
    );
 };
